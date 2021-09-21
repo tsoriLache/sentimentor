4ec2517d5e73d6b4a725fd133e52d56b99c24db1
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-09-21 22:01:53 +0300

    IMPROVMENTS AND STYLING

--- sentimentor.js
+++ sentimentor.js
@@ -11,23 +11,29 @@ async function sendTextToSentimentorAPI(element){
         },
         body : JSON.stringify({ "text":  text })
     })
+    document.querySelector("#http-cat > img").src = `https://http.cat/${sentiment.status}`
     return await sentiment.json();
 }
 
 async function getTextSentim(){
     try{
-        // let loading = createElement("p",["loading"],[],{id:"loading"})
-        document.getElementById("result-section").insertAdjacentHTML("afterbegin" ,'<div class="container"><span class="circle"></span><span class="circle"></span><span class="circle"></span><span class="circle"></span> </div>');
+        if(document.querySelector("#result-section > p.answer")){
+            document.querySelector("#result-section > p.answer").remove();
+        }
+        document.getElementById("result-section").insertAdjacentHTML("afterbegin" ,'<div class="container"><span class="circle"></span><span class="circle"></span><span class="circle"></span> </div>');
         let apiAnalysis = await sendTextToSentimentorAPI();
+        console.log(apiAnalysis)
         let polarity = apiAnalysis.result.polarity;
         let classOfAnalysis= polarity===0? "neutral":polarity>0? "positive":"negative";
         let type = apiAnalysis.result.type;
-        let analysis = createElement("p",[createElement("span",[type],[classOfAnalysis]), createElement("span",[polarity],[classOfAnalysis])],["answer"])
+        const text = document.querySelector("#input").value;
+        let analysis = createElement("p",["Your Text: ",createElement("span",[text],["input-text"]),"Type: ",createElement("span",[type],[classOfAnalysis]),"Polarity: " ,createElement("span",[polarity],[classOfAnalysis])],["answer"])
         document.querySelector("#result-section > div").remove();
         document.querySelector("#result-section").append(analysis);
     }
     catch{
-        let error=createElement("p",["there was an error"])
+        let error=createElement("p",["There Was An Error...try to fill the textbox"],["answer"])
+        document.querySelector("#result-section > div").remove();
         document.querySelector("#result-section").append(error);
     }
 }
